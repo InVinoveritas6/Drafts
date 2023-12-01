e05d2d23f591973314dcd5a44014f1bb07c68dd8
SHATE\Lahodzich <sagodich@mail.ru>
2023-12-01 17:53:01 +0300

    2

--- src/components/Quill/Quill.jsx
+++ src/components/Quill/Quill.jsx
@@ -5,11 +5,18 @@ import "react-quill/dist/quill.snow.css";
 
 const Quill = () => {
   var toolbarOptions = [
-    ["bold", "italic", "underline"],
-    ["link", "image"],
-    [{ size: [] }],
-    [{'color': []}],
-    [{ 'align': [] }],
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
   ];
 
   const module = {
